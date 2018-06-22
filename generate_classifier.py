77d9df3b254cd10386b417d243a9d40058297c31
Benjamin Lee <benjamindlee@me.com>
2018-06-22 16:17:46 -0600

    Automatic categorical encoding

--- generate_classifier.py
+++ generate_classifier.py
@@ -9,14 +9,10 @@ import click
 def cli(erv_data):
     # import the ERV expression data as a Pandas dataframe
     df = pd.read_csv(erv_data)
-    df = df.values[:, 1:]
-
-    # create a class vector corresponding to the df being 50/50 split of each class
-    classes = [0] * int(len(df) / 2)
-    classes.extend([1] * len(classes))
+    df["class"] = df["class"].astype("category").cat.codes
 
     # create the test and training data
-    X_train, X_test, y_train, y_test = train_test_split(df, classes, train_size=0.75, test_size=0.25)
+    X_train, X_test, y_train, y_test = train_test_split(df.values[:, 1:-1], df.values[:, -1], train_size=0.75, test_size=0.25)
 
     # convert them all to floats
     X_train, X_test, y_train, y_test = X_train.astype(float), X_test.astype(float), y_train.astype(float), y_test.astype(float)
