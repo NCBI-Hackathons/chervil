2d7bc13413c574b3efe9e4a06255cfe7918d69c5
Benjamin Lee <benjamindlee@me.com>
2018-06-22 17:30:46 -0600

    remove first column slicing

--- generate_classifier.py
+++ generate_classifier.py
@@ -12,7 +12,7 @@ def cli(erv_data):
     df["class"] = df["class"].astype("category").cat.codes
 
     # create the test and training data
-    X_train, X_test, y_train, y_test = train_test_split(df.values[:, 1:-1], df.values[:, -1], train_size=0.75, test_size=0.25)
+    X_train, X_test, y_train, y_test = train_test_split(df.values[:, :-1], df.values[:, -1], train_size=0.75, test_size=0.25)
 
     # convert them all to floats
     X_train, X_test, y_train, y_test = X_train.astype(float), X_test.astype(float), y_train.astype(float), y_test.astype(float)
