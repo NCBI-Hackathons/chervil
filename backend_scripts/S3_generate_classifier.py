c4b6b11eef454793e5b40910259aef41a1fa19eb
Benjamin Lee <benjamindlee@me.com>
2018-06-23 13:43:23 -0600

    Add PyCM output from pipeline

--- backend_scripts/S3_generate_classifier.py
+++ backend_scripts/S3_generate_classifier.py
@@ -5,12 +5,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from tpot import TPOTClassifier
 import click
+from pycm import *
 
 @click.command()
 @click.argument("erv_data", type=click.Path(exists=True, dir_okay=False))
 def cli(erv_data):
     # import the ERV expression data as a Pandas dataframe
     df = pd.read_csv(erv_data)
+    class_codes = dict(enumerate(df['class'].astype("category").cat.categories))
     df["class"] = df["class"].astype("category").cat.codes
 
     # create the test and training data
@@ -18,8 +20,15 @@ def cli(erv_data):
 
     # convert them all to floats
     X_train, X_test, y_train, y_test = X_train.astype(float), X_test.astype(float), y_train.astype(float), y_test.astype(float)
-    pipeline_optimizer = TPOTClassifier(generations=5, population_size=20, cv=5, verbosity=2)
+
+    # create a pipeline
+    pipeline_optimizer = TPOTClassifier(generations=1, population_size=1, cv=5, verbosity=2)
     pipeline_optimizer.fit(X_train, y_train)
+    pipeline_optimizer.export('tpot_exported_pipeline.py')
+
+    print(f"Validation Accuracy: {pipeline_optimizer.score(X_test, y_test)}")
+    cm = ConfusionMatrix([class_codes[y] for y in y_test], [class_codes[y] for y in [pipeline_optimizer.predict(x.reshape(1, -1))[0] for x in X_test]])
+    cm.save_html("report")
 
 
 if __name__ == '__main__':
