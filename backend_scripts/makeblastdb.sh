8f2a29fd71b4856ca782d47ab19eac6124710692
Jeremy Ash <jrash@users.noreply.github.com>
2018-06-23 14:03:58 -0400

    Update makeblastdb.sh

--- backend_scripts/makeblastdb.sh
+++ backend_scripts/makeblastdb.sh
@@ -2,4 +2,4 @@
 
 REF_FASTA=$1
 
-makeblastdb -in $REF_FASTA  -dbtype nucl -parse_seqids -out viruses/blast_dbs/referencedb
+makeblastdb -in $REF_FASTA  -dbtype nucl -parse_seqids -out blast_dbs/referencedb
